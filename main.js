ab1fbb6e2635c29501cb66db71954af92ed50b8e
SRShaikh <srshaikh.work@gmail.com>
2021-12-04 08:59:58 +0530

    checking active status in menubar

--- main.js
+++ main.js
@@ -10,8 +10,34 @@
     });
   }
 
+  // ------ Navigation active state on scroll -------------
+  var nav_sections = $('section');
+  var main_nav = $('.nav-menu');
 
-  // $('.tooltip-test').tooltip();
+  $(window).on('scroll', function () {
+    var current_pos = $(this).scrollTop();
+
+    nav_sections.each(function () {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight() - 40;
+
+      console.log("Current Position: " + current_pos);
+
+      if (current_pos >= top && current_pos <= bottom) {
+        if (current_pos <= bottom) {
+          main_nav.find("li").removeClass('active');
+          // console.log("Is on remove active");
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+      }
+      if (current_pos == 40) {
+        $('html, body').scrollTop(0);
+        $(".menu-bar ul li:first").addClass('active');
+      }
+
+    })
+  });
+
 
   $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
